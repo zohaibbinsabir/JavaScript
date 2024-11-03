cec522ccf84fcbe02aa6a7906608f9daf6e43fd1
zohaibbinsabir <zohaibbinsabir@gmail.com>
2024-11-03 12:53:07 +0500

    class 10 complete

---
+++ Class 10 - Library.js
@@ -0,0 +1,13 @@
+export let num = 10;
+function sum(a,b){
+    return a+b;
+}
+class obj{
+    name;
+    class;
+}
+export default function()
+{
+    console.log("zohaib");
+}
+export {sum,obj};
---
+++ Class 10 - Modules.js
@@ -0,0 +1,5 @@
+import {num, sum, obj, default as Zohaib} from './Class 10 - Library.js'
+console.log(sum(1,2));
+let object = new obj();
+console.log(object);
+Zohaib();
